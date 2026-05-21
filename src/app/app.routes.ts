96b9ed41dddb36ddc173c4a5bcd7f250993fd9a9
hellstaff98 <aleksey_2907@mail.ru>
2026-05-21 23:15:33 +0300

    fix: lazy loading or users bundle

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import { USERS_ROUTES } from '@routes/users.routes';
 
 export const routes: Routes = [
-  { path: 'users', loadChildren: () => import('./routes/users.routes').then((m) => USERS_ROUTES) },
+  {
+    path: 'users',
+    loadChildren: () => import('./routes/users.routes').then((m) => m.USERS_ROUTES),
+  },
   { path: '**', redirectTo: 'users' },
 ];
